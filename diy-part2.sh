f24bfc81e6530b4e274d57c97b595e4d767beda8
githzchen <59717746+githzchen@users.noreply.github.com>
2021-02-23 22:02:06 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,7 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
 rm -rf package/lean/luci-theme-argon
-cd openwrt/files
-ln -s /opt/jd jd
-cd etc
-ln -s /opt/verysync verysync
+ls
